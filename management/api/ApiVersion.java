c8fb93ce6b7e14b9e9a156fbbc6f3b0ce5cab736
Sazwan <75355470+SazuSazu@users.noreply.github.com>
2024-12-16 22:37:37 +0800

    Revert "Add files via upload"

--- management/api/ApiVersion.java
+++
@@ -1,5 +0,0 @@
-package com.example.management.api;
-
-public interface ApiVersion {
-    String getVer();
-}
--- management/service/course/CourseService.java
+++
@@ -1,4 +0,0 @@
-package com.example.management.service.course;
-
-public interface CourseService {
-}
